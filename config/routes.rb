a99132fe24cbe0b00890ffd3433dd352e8f56826
Jian Weihang <tonytonyjan@gmail.com>
2018-04-01 17:14:29 +0800

    feat: add answers controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'questions#index'
   get 'oauth2/callback'
   post 'sign_out' => 'sessions#sign_out'
-  resources :questions, except: :index
+  resources :questions, except: :index do
+    resources :answers, except: %i[index show]
+  end
 end
